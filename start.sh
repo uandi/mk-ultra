79de6111f2ee8f56d352b2d4d50784e137fd4c57
Shady Tawfik <stawfik@uandi.com>
2024-10-02 21:53:06 +0200

    script more portable and user-friendly

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Script to activate virtual environment and start ComfyUI
 
+# Navigate to the script's directory
+cd "$(dirname "$0")"
+
 # Activate the virtual environment
 source venv/bin/activate
 
